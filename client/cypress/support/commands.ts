2deda5482f1f3615caedde55b7b48bbfee20fe1d
Jessica Fealy <jessicaf091@outlook.com>
2024-05-11 23:08:34 +0100

    remove unnecessary click

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -14,7 +14,6 @@ Cypress.Commands.add("login", () => {
     cy.get('input[name="username"]').type(Cypress.env("CYPRESS_USERNAME"));
     cy.get('input[name="password"]').type(Cypress.env("CYPRESS_PASSWORD"));
     cy.get('button[name="action"]').click();
-    cy.get('button[name="action"]').click();
   });
 
   cy.findByText(Cypress.env("CYPRESS_USERNAME"));
