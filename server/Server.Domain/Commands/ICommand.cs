ed68597fc26f052fc1b6ae3354920d08a3cba3e0
Jessica Fealy <jessicaf091@outlook.com>
2024-05-12 23:01:54 +0100

    setup mediator

---
+++ server/Server.Domain/Commands/ICommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Domain.Commands;
+
+using MediatR;
+
+public interface ICommand<TDBContext> : IRequest
+    where TDBContext : BookRepoContext
+{
+    Task Execute(TDBContext context, IPublisher publisher, CancellationToken cancellationToken);
+}
+
+public interface ICommand : ICommand<BookRepoContext> { }
