009b0c4aa772884c8dcdab9c7d91db0f730fd34c
Jessica Fealy <jessicaf091@outlook.com>
2024-05-17 21:43:09 +0100

    Setup customer summary tests

--- server/cypress/e2e/customer.cy.ts
+++ server/cypress/e2e/customer.cy.ts
@@ -12,33 +12,27 @@ describe("Customer", () => {
   });
   describe("get-customer-summary", () => {
     it("should return a customer summary and 200", () => {
-      cy.login().then((res) => {
-        const sub = decode(res.body.access_token)?.sub;
-        const bearerToken = `${res.body.token_type} ${res.body.access_token}`;
-        console.log(res);
-
+      cy.request({
+        method: "POST",
+        url: "/action/setup-customer",
+        headers: {
+          authorization: bearerToken,
+        },
+        body: {
+          id: sub,
+          includeDefaultBookshelves: true,
+        },
+      }).then((res) => {
+        assert.isTrue(res.isOkStatusCode);
         cy.request({
-          method: "POST",
-          url: "/action/setup-customer",
+          method: "GET",
+          url: "/customer/get-customer-summary",
           headers: {
             authorization: bearerToken,
           },
-          body: {
-            id: sub,
-            includeDefaultBookshelves: true,
-          },
         }).then((res) => {
           assert.isTrue(res.isOkStatusCode);
-          cy.request({
-            method: "GET",
-            url: "/customer/get-customer-summary",
-            headers: {
-              authorization: bearerToken,
-            },
-          }).then((res) => {
-            assert.isTrue(res.isOkStatusCode);
-            assert.isNotNull(res.body);
-          });
+          assert.isNotNull(res.body);
         });
       });
     });
