38709af05036f25cf73355467c0af9c533d8b241
Jessica Fealy <jessicaf091@outlook.com>
2024-05-18 18:29:59 +0100

    Add redirect for new customers on the dashboard

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import { useGetCustomerSummary } from "@/hooks";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { Button } from "@jecfe/react-design-system";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default function Home() {
+export default function Dashboard() {
+  const { isLoading, data, error } = useGetCustomerSummary();
   const router = useRouter();
   const { user } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !data) {
+      router.push("/dashbord/setup-profile");
+    }
+  }, [data, error]);
+
   return (
     <div className="flex min-h-screen w-full flex-col items-center pt-10 md:justify-center md:pt-0">
       <h1 className="flex flex-row pt-0 text-center text-5xl font-bold tracking-tight text-slate-200 md:pt-20 md:text-8xl">
