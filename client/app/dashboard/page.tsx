0a8a170cddf2a89718c3de3c2209005fdb147c11
Jessica Fealy <jessicaf091@outlook.com>
2024-05-19 16:50:24 +0100

    Update to have a spinner on the dashboard when loading in customer summary

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useGetCustomerSummary } from "@/hooks";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { Button } from "@jecfe/react-design-system";
+import { Button, Spinner } from "@jecfe/react-design-system";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -17,24 +17,36 @@ export default function Dashboard() {
     }
   }, [data, error]);
 
-  return (
-    <div className="flex min-h-screen w-full flex-col items-center pt-10 md:justify-center md:pt-0">
-      <h1 className="flex flex-row pt-0 text-center text-5xl font-bold tracking-tight text-slate-200 md:pt-20 md:text-8xl">
-        Welcome
-      </h1>
-      <div className="mt-4 flex max-w-sm flex-row text-center text-xl font-bold tracking-tight text-slate-400 md:max-w-full md:text-3xl">
-        {user?.name}
+  if (isLoading && !data) {
+    return (
+      <div className="flex min-h-screen w-full flex-col items-center pt-10 md:justify-center md:pt-0">
+        <div className="flex items-center justify-center">
+          <Spinner fast={isLoading} size="large" />
+        </div>
       </div>
-      <Button
-        onClick={() => {
-          router.push("/api/auth/logout");
-        }}
-        variant="secondary"
-        size="large"
-        className="my-12"
-      >
-        Logout
-      </Button>
-    </div>
-  );
+    );
+  }
+
+  if (!isLoading && data) {
+    return (
+      <div className="flex min-h-screen w-full flex-col items-center pt-10 md:justify-center md:pt-0">
+        <h1 className="flex flex-row pt-0 text-center text-5xl font-bold tracking-tight text-slate-200 md:pt-20 md:text-8xl">
+          Welcome
+        </h1>
+        <div className="mt-4 flex max-w-sm flex-row text-center text-xl font-bold tracking-tight text-slate-400 md:max-w-full md:text-3xl">
+          {user?.name}
+        </div>
+        <Button
+          onClick={() => {
+            router.push("/api/auth/logout");
+          }}
+          variant="secondary"
+          size="large"
+          className="my-12"
+        >
+          Logout
+        </Button>
+      </div>
+    );
+  }
 }
