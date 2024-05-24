ee7a3d41c665242ba75cb716c507b3c6539614fa
Jessica Fealy <jessicaf091@outlook.com>
2024-05-24 21:51:47 +0100

    Added additional functionality to create customer

--- server/Server.Domain/Commands/SetupCustomerCommand.cs
+++ server/Server.Domain/Commands/SetupCustomerCommand.cs
@@ -1,6 +1,5 @@
 namespace Server.Domain.Commands;
 
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Server.Domain;
 using Server.Domain.Models;
@@ -8,6 +7,8 @@ using Server.Domain.Models;
 public class SetupCustomerCommand : ICommand<BookRepoContext>
 {
     public required string Id { get; init; }
+    public List<string>? BookshelvesNames { get; init; }
+    public List<Book>? Books { get; init; }
     public bool IncludeDefaultBookshelves { get; init; }
 
     public async Task Execute(
@@ -25,10 +26,23 @@ public class SetupCustomerCommand : ICommand<BookRepoContext>
 
         Customer customer = new() { Id = Id, CreationDate = ctx.time.GetUtcNow(), };
 
+        var homelessBookBookshelf = new Bookshelf()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Homeless Books",
+            CreationDate = ctx.time.GetUtcNow(),
+            UpdatedDate = ctx.time.GetUtcNow(),
+        };
+        if (Books is { })
+        {
+            customer.Bookshelves =  [ ..customer.Bookshelves, homelessBookBookshelf ];
+        }
+
         if (IncludeDefaultBookshelves)
         {
             customer.Bookshelves =
             [
+                ..customer.Bookshelves,
                 new (){
                     Id = Guid.NewGuid(),
                     Name = "Wanting to read",
@@ -54,7 +68,44 @@ public class SetupCustomerCommand : ICommand<BookRepoContext>
             ];
         }
 
+        if (BookshelvesNames is { } names)
+        {
+            List<Bookshelf> customBookshelves =  [ ];
+            foreach (var name in names)
+            {
+                customBookshelves.Add(
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = name,
+                        CreationDate = DateTimeOffset.UtcNow
+                    }
+                );
+            }
+            customer.Bookshelves =  [ ..customer.Bookshelves, ..customBookshelves ];
+        }
+
         dbContext.Customer.Add(customer);
+
+        if (Books is { } books)
+        {
+            dbContext.Books.AddRange(Books);
+            List<BookshelfBook> bookshelfBooks =  [ ];
+            for (int i = 0; i < books.Count; i++)
+            {
+                bookshelfBooks.Add(
+                    new BookshelfBook()
+                    {
+                        BookshelfId = homelessBookBookshelf.Id,
+                        Isbn = books[i].Isbn,
+                        Order = i,
+                        Book = books[i],
+                        Bookshelf = homelessBookBookshelf
+                    }
+                );
+            }
+            dbContext.BookshelfBook.AddRange(bookshelfBooks);
+        }
 
         await dbContext.SaveChangesAsync(cancellationToken);
     }
