9449b181bbe7cae812afdcc444752e23cda6c574
Jessica Fealy <jessicaf091@outlook.com>
2024-05-25 16:44:24 +0100

    Additional updates

--- server/Server/OpenLibrary/OpenLibraryClient.cs
+++ server/Server/OpenLibrary/OpenLibraryClient.cs
@@ -1,11 +1,44 @@
-using Server.Domain.Models;
-
 namespace Server.OpenLibrary;
 
-public class OpenLibraryClient() : IOpenLibraryCient
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using OpenLibraryNET;
+using OpenLibraryNET.Data;
+using OpenLibraryNET.Utility;
+using Server.Domain.Models;
+
+public class OpenLibraryClient : IOpenLibraryCient
 {
-    public Task<Book?> GetBook(string isbn, CancellationToken cancellationToken)
+    private OpenLibraryNET.OpenLibraryClient _client;
+
+    public OpenLibraryClient()
     {
-        throw new NotImplementedException();
+        _client = new OpenLibraryNET.OpenLibraryClient();
+    }
+
+    public async Task<Book?> GetBook(string isbn, CancellationToken cancellationToken)
+    {
+        OLEditionData? book = await _client.Edition.GetDataByISBNAsync(isbn);
+
+        if (book is null)
+        {
+            return null;
+        }
+
+        var pictureUri = OpenLibraryUtility.BuildCoversUri(
+            CoverIdType.ISBN,
+            isbn,
+            ImageSize.Medium
+        );
+
+        return new Book()
+        {
+            Isbn = isbn,
+            Name = book.Title,
+            Release = "Unknown",
+            Picture = pictureUri.ToString(),
+            Subjects =  [ .. book.Subjects ],
+            PageCount = book.PageCount
+        };
     }
 }
