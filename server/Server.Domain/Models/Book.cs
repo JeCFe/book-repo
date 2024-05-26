1671c86c22db5011fa5917348cd63d3318ae0eb5
Jessica Fealy <jessicaf091@outlook.com>
2024-05-26 01:02:09 +0100

    Update book

--- server/Server.Domain/Models/Book.cs
+++ server/Server.Domain/Models/Book.cs
@@ -4,9 +4,9 @@ public record Book
 {
     public required string Isbn { get; init; }
     public required string Name { get; set; }
-    public List<string> Authors { get; set; } = [ ];
+    public List<string>? Authors { get; set; } = [ ];
     public List<string> Subjects { get; set; } = [ ];
-    public required string Release { get; set; }
-    public required string Picture { get; set; }
+    public string? Release { get; set; }
+    public string? Picture { get; set; }
     public int PageCount { get; init; }
 }
