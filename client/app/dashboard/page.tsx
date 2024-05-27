6eabca6c8d857b4d4cc0169d2e945eddde9dcd35
Jessica Fealy <jessicaf091@outlook.com>
2024-05-27 13:20:48 +0100

    Made copy of logo so it will render when otheri is hidden

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BookLogo } from "@/assets";
+import { BookLogo, BookLogoCopy } from "@/assets";
 import { SideNav } from "@/components";
 import { useGetCustomerSummary } from "@/hooks";
 import { useUser } from "@auth0/nextjs-auth0/client";
@@ -34,13 +34,7 @@ export default function Dashboard() {
       <div>
         <div className="flex w-full">
           <SideNav>
-            <div className="flex h-full flex-col divide-y divide-slate-500 py-2 pt-4">
-              <div className="pointer-events-none flex flex-col items-center justify-center pb-4 pt-4">
-                <BookLogo height="100" />
-                <div className="text-lg font-bold tracking-tight text-slate-200">
-                  The Book Repository
-                </div>
-              </div>
+            <>
               <div className="flex w-full flex-grow flex-row overflow-y-auto px-2"></div>
               <div className="flex flex-row px-2 pt-2 text-center text-sm">
                 <div className="flex items-center text-center text-slate-400">
@@ -49,7 +43,7 @@ export default function Dashboard() {
                 <div className="flex flex-grow" />
                 <Anchor href="/api/auth/logout">Logout</Anchor>
               </div>
-            </div>
+            </>
           </SideNav>
           <div className="flex w-full flex-col py-24 pl-12 text-slate-500">
             test
