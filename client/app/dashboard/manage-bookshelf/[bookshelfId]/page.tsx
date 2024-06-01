56b775c4bef4a0f810bd06a1eedd72ab7a44311b
Jessica Fealy <jessicaf091@outlook.com>
2024-06-01 14:50:36 +0100

    Added init table onto bookshelf page

--- client/app/dashboard/manage-bookshelf/[bookshelfId]/page.tsx
+++ client/app/dashboard/manage-bookshelf/[bookshelfId]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Table } from "@/components";
 import { useGetBookshelf } from "@/hooks/useGetBookshelf";
 import { Spinner } from "@jecfe/react-design-system";
 import { useRouter } from "next/navigation";
@@ -25,7 +26,7 @@ export default function ManageBookshelf({
     }
   }, [data, isLoading]);
 
-  if (isLoading && !data) {
+  if (isLoading && data === undefined) {
     return (
       <div className="flex min-h-screen w-full flex-col items-center pt-10 md:justify-center md:pt-0">
         <div className="flex items-center justify-center">
@@ -33,14 +34,39 @@ export default function ManageBookshelf({
         </div>
       </div>
     );
+  } else {
+    return (
+      <div className="text-slate-400">
+        <h1 className="flex flex-col pb-4 text-5xl font-bold tracking-tight text-slate-200 md:text-8xl">
+          {`Manage ${data?.name}`}
+        </h1>
+        <div className="flex overflow-auto">
+          <Table>
+            <thead>
+              <tr>
+                <th>Order</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Rating</th>
+                <td>ISBN</td>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.books?.map((book, i) => (
+                <tr key={`${book.book.name}-${i}`}>
+                  <td>{book.order}</td>
+                  <td>{book.book.name}</td>
+                  <td>{book.book.authors?.join(", ")}</td>
+                  <td>COMING</td>
+                  <td>{book.book.isbn}</td>
+                  <td>DELETE COMING</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    );
   }
-
-  return (
-    <div className="text-slate-400">
-      <h1 className="flex flex-col pb-4 text-5xl font-bold tracking-tight text-slate-200 md:text-8xl">
-        {`Manage ${data?.name}`}
-      </h1>
-      <div>{JSON.stringify(data)}</div>
-    </div>
-  );
 }
