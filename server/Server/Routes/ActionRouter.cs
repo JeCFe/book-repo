6a331ce592eb0412bde122d933e4599e8840f1bd
Jessica Fealy <jessicaf091@outlook.com>
2024-06-02 16:12:11 +0100

    Add new route

--- server/Server/Routes/ActionRouter.cs
+++ server/Server/Routes/ActionRouter.cs
@@ -76,6 +76,27 @@ public static class ActionRouter
         return TypedResults.Ok();
     }
 
+    private static async Task<Results<NoContent, NotFound, ForbidHttpResult>> RemoveBookshelfBook(
+        RemoveBookshelfBookCommand command,
+        IMediator mediator,
+        IUserContext userContext,
+        CancellationToken cancellationToken
+    )
+    {
+        var userId = userContext.UserId;
+        if (userId is not { } || command.CustomerId != userId)
+        {
+            return TypedResults.Forbid();
+        }
+
+        var amount = await mediator.Send(command, cancellationToken);
+        if (amount == 0)
+        {
+            return TypedResults.NotFound();
+        }
+        return TypedResults.NoContent();
+    }
+
     public static RouteGroupBuilder MapActionEndpoints(this RouteGroupBuilder group)
     {
         group.WithTags("Actions");
