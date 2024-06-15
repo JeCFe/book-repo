5d5a1abfda6eb94cea86aaabd5c9bd7fbc5a8b4f
Jessica Fealy <jessicaf091@outlook.com>
2024-06-15 14:19:51 +0100

    Update db models

--- server/Server.Domain/Models/BookshelfBook.cs
+++ server/Server.Domain/Models/BookshelfBook.cs
@@ -3,8 +3,10 @@ namespace Server.Domain.Models;
 public class BookshelfBook()
 {
     public required Guid BookshelfId { get; init; }
-    public required string Isbn { get; init; }
+    public required Guid CustomerBookId { get; init; }
     public required int Order { get; set; }
+    public required string Isbn { get; init; }
+
     public required Bookshelf Bookshelf { get; init; }
-    public required Book Book { get; init; }
+    public required CustomerBook CustomerBook { get; init; }
 }
---
+++ server/Server.Domain/Models/CustomerBook.cs
@@ -0,0 +1,11 @@
+namespace Server.Domain.Models;
+
+public class CustomerBook()
+{
+    public required Guid Id { get; init; }
+    public required string CustomerId { get; init; }
+    public required string Isbn { get; init; }
+    public int Ranking { get; set; }
+    public required Customer Customer { get; init; }
+    public required Book Book { get; init; }
+}
