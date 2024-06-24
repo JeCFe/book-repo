87eb5b3b6beaff3946da1169f6eccede381358c2
Jessica Fealy <jessicaf091@outlook.com>
2024-06-24 22:51:54 +0100

    Added new hook

--- client/hooks/index.ts
+++ client/hooks/index.ts
@@ -3,6 +3,7 @@ export * from "./addSetupWizard";
 export * from "./useGetBook";
 export * from "./useGetBookshelf";
 export * from "./useGetBookshelfSummary";
+export * from "./useGetCustomerBook";
 export * from "./useGetCustomerSummary";
 export * from "./useGetHomelessId";
 export * from "./useSearchForBooks";
