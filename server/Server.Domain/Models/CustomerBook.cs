342f535912addd7d06a5001c685b9aa32fd34359
Jessica Fealy <jessicaf091@outlook.com>
2024-06-27 02:09:29 +0100

    Update customer book

--- server/Server.Domain/Models/CustomerBook.cs
+++ server/Server.Domain/Models/CustomerBook.cs
@@ -6,6 +6,7 @@ public class CustomerBook()
     public required string CustomerId { get; init; }
     public required string Isbn { get; init; }
     public int Ranking { get; set; }
+    public string? Comment { get; set; }
     public required Customer Customer { get; init; }
     public required Book Book { get; init; }
 }
