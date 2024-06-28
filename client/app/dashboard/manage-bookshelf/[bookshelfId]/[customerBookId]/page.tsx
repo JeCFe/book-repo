2533a125ecdc9d98b68dff17f11cf55d8f382921
Jessica Fealy <jessicaf091@outlook.com>
2024-06-28 05:59:19 +0100

    Add begining details

--- client/app/dashboard/manage-bookshelf/[bookshelfId]/[customerBookId]/page.tsx
+++ client/app/dashboard/manage-bookshelf/[bookshelfId]/[customerBookId]/page.tsx
@@ -1,10 +1,35 @@
+import { useGetCustomerBook } from "@/hooks";
 import { UserProfile, withPageAuthRequired } from "@auth0/nextjs-auth0/client";
+import { Anchor } from "@jecfe/react-design-system";
 
 type Props = {
-  params: { customerBookId: string };
+  params: { customerBookId: string; bookshelfId: string };
 };
 
 export default withPageAuthRequired(function ManageBook({
   params,
   user,
-}: Props & { user: UserProfile }) {});
+}: Props & { user: UserProfile }) {
+  const { data, isLoading } = useGetCustomerBook(
+    user.sub!,
+    params.customerBookId,
+  );
+
+  return (
+    <div>
+      <div className="flex flex-row space-x-2">
+        <Anchor href="/dashboard" className="pb-6">{`< Dashboard`}</Anchor>
+        <Anchor
+          href={`/dashboard/manage-bookshelf/${encodeURIComponent(params.bookshelfId)}`}
+          className="pb-6"
+        >
+          {"< Manage Bookshelf"}
+        </Anchor>
+        <div className="underline underline-offset-4">{"< Manage Book"}</div>
+      </div>
+      <h1 className="flex flex-col pb-4 text-5xl font-bold tracking-tight text-slate-200 md:text-8xl">
+        {`Manage Book`}
+      </h1>
+    </div>
+  );
+});
