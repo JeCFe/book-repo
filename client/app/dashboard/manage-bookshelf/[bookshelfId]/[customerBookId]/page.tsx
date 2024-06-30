ed81e147cca1bd4abfecabfeb8cc0fafdd270e21
Jessica Fealy <jessicaf091@outlook.com>
2024-06-30 15:23:26 +0100

    Add in comment render section

--- client/app/dashboard/manage-bookshelf/[bookshelfId]/[customerBookId]/page.tsx
+++ client/app/dashboard/manage-bookshelf/[bookshelfId]/[customerBookId]/page.tsx
@@ -5,7 +5,6 @@ import { useGetCustomerBook } from "@/hooks";
 import { updateRanking } from "@/services";
 import { UserProfile, withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import { Anchor, Spinner } from "@jecfe/react-design-system";
-import { ReactNode } from "react";
 import toast from "react-hot-toast";
 
 type Props = {
@@ -70,51 +69,60 @@ export default withPageAuthRequired(function ManageBook({
             </div>
           </div>
         ) : (
-          <div className="mt-4 flex flex-col space-y-2 md:flex-row md:space-x-12 md:space-y-0">
-            <div>
-              <Picture
-                size="xLarge"
-                pictureUrl={data.book.picture}
-                title={data.book.name ?? ""}
-                loading={isLoading}
-              />
-            </div>
-
-            <div className="flex flex-col space-y-2">
-              <RenderSection size="large" theme="dark" title="Book Title">
-                {data.book.name}
-              </RenderSection>
-              <RenderSection size="large" theme="dark" title="Rating">
-                <RenderStar
-                  allowHover
-                  ranking={data.ranking}
-                  onChange={updateBookRanking}
+          <div className="flex flex-col">
+            <div className="mt-4 flex flex-col space-y-2 md:flex-row md:space-x-12 md:space-y-0">
+              <div>
+                <Picture
+                  size="xLarge"
+                  pictureUrl={data.book.picture}
+                  title={data.book.name ?? ""}
+                  loading={isLoading}
                 />
-              </RenderSection>
-              <RenderSection size="large" theme="dark" title="Release Year">
-                {data.book.release}
-              </RenderSection>
+              </div>
 
-              {data.book.pageCount && data.book.pageCount > 0 && (
-                <RenderSection size="large" theme="dark" title="Page Count">
-                  <div className="max-w-sm">{data.book.pageCount}</div>
+              <div className="flex flex-col space-y-2">
+                <RenderSection size="large" theme="dark" title="Book Title">
+                  {data.book.name}
                 </RenderSection>
-              )}
+                <RenderSection size="large" theme="dark" title="Rating">
+                  <RenderStar
+                    allowHover
+                    ranking={data.ranking}
+                    onChange={updateBookRanking}
+                  />
+                </RenderSection>
+                <RenderSection size="large" theme="dark" title="Release Year">
+                  {data.book.release}
+                </RenderSection>
 
-              {data.book.authors && data.book.authors.length > 0 && (
-                <RenderSection size="large" theme="dark" title="Authors">
-                  <div className="max-w-sm">
-                    {data.book.authors?.join(", ")}
-                  </div>
-                </RenderSection>
-              )}
-              {data.book.subjects && data.book.subjects.length > 0 && (
-                <RenderSection size="large" theme="dark" title="Subjects">
-                  <div className="max-w-sm">
-                    {data.book.subjects?.join(", ")}
-                  </div>
-                </RenderSection>
-              )}
+                {data.book.pageCount && data.book.pageCount > 0 && (
+                  <RenderSection size="large" theme="dark" title="Page Count">
+                    <div className="max-w-sm">{data.book.pageCount}</div>
+                  </RenderSection>
+                )}
+
+                {data.book.authors && data.book.authors.length > 0 && (
+                  <RenderSection size="large" theme="dark" title="Authors">
+                    <div className="max-w-sm">
+                      {data.book.authors?.join(", ")}
+                    </div>
+                  </RenderSection>
+                )}
+                {data.book.subjects && data.book.subjects.length > 0 && (
+                  <RenderSection size="large" theme="dark" title="Subjects">
+                    <div className="max-w-sm">
+                      {data.book.subjects?.join(", ")}
+                    </div>
+                  </RenderSection>
+                )}
+              </div>
+            </div>
+            <div className="mt-10">
+              <RenderSection
+                size="large"
+                theme="dark"
+                title="Comment"
+              ></RenderSection>
             </div>
           </div>
         )}
