3181a1f3b635a075290f869573368576d4f6aa8d
Jessica Fealy <jessicaf091@outlook.com>
2024-07-02 20:06:11 +0100

    Add new hook

--- client/hooks/index.ts
+++ client/hooks/index.ts
@@ -4,6 +4,7 @@ export * from "./useGetBook";
 export * from "./useGetBookshelf";
 export * from "./useGetBookshelfSummary";
 export * from "./useGetCustomerBook";
+export * from "./useGetCustomerBooks";
 export * from "./useGetCustomerSummary";
 export * from "./useGetHomelessId";
 export * from "./useSearchForBooks";
---
+++ client/hooks/useGetCustomerBooks.ts
@@ -0,0 +1,27 @@
+import { getApiClient } from "@/services";
+import useSWR from "swr";
+
+const getCustomerBooks = getApiClient()
+  .path("/customer/books")
+  .method("get")
+  .create();
+
+export const useGetCustomerBooks = () => {
+  const { data, error, isLoading, mutate } = useSWR(
+    "getCustomerBooks",
+    async () => (await getCustomerBooks({})).data,
+    {
+      refreshInterval: 60000,
+      revalidateOnFocus: false,
+      onErrorRetry: (error) => {
+        if (error.status === 404) return;
+      },
+    },
+  );
+  return {
+    data,
+    error,
+    isLoading,
+    mutate,
+  };
+};
