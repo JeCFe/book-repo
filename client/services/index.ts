1cc92d4939fb4c93422573808737dbf544badd1a
Jessica Fealy <jessicaf091@outlook.com>
2024-07-03 23:02:02 +0100

    Refactors

--- client/services/index.ts
+++ client/services/index.ts
@@ -2,5 +2,8 @@ export * from "./addAuth";
 export * from "./addBaseUrl";
 export * from "./getApiClient";
 export * from "./getEndpoints";
+export * from "./removeBookshelf";
+export * from "./removeBookshelfBook";
+export * from "./updateBookshelfOrder";
 export * from "./updateComment";
 export * from "./updateRanking";
