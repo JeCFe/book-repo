23888d93b7b985515d3ac94609e5b8d3578e1f1b
Jessica Fealy <jessicaf091@outlook.com>
2024-07-17 21:26:07 +0100

    Create data model

---
+++ server/Server.Domain/Models/Shareable.cs
@@ -0,0 +1,22 @@
+namespace Server.Domain.Models;
+
+public record Shareable
+{
+    public required Guid Id { get; init; }
+    public required Customer Customer { get; init; }
+    public List<ShareableBookshelf>? Bookshelves { get; init; } = [ ];
+    public ShareableBookShowcase? Showcase { get; init; } = null;
+}
+
+public record ShareableBookshelf
+{
+    public required Bookshelf Bookshelves { get; init; }
+    public required int Order { get; init; }
+}
+
+public record ShareableBookShowcase
+{
+    public required CustomerBook CustomerBook { get; init; }
+    public bool ShowRanking { get; init; } = true;
+    public bool ShowComment { get; init; } = true;
+}
