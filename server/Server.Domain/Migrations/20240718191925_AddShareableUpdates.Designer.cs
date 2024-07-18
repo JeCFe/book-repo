ab07deb4622dc68623e40f7e244727abb4e80cd7
Jessica Fealy <jessicaf091@outlook.com>
2024-07-18 20:19:59 +0100

    Db migrations updates

--- server/Server.Domain/Migrations/20240718191925_AddShareableUpdates.Designer.cs
+++ server/Server.Domain/Migrations/20240718191925_AddShareableUpdates.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Server.Domain;
 
@@ -11,9 +12,11 @@ using Server.Domain;
 namespace Server.Domain.Migrations
 {
     [DbContext(typeof(BookRepoContext))]
-    partial class BookRepoContextModelSnapshot : ModelSnapshot
+    [Migration("20240718191925_AddShareableUpdates")]
+    partial class AddShareableUpdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
