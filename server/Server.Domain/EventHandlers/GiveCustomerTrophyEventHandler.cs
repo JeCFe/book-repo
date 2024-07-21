8486a619a625fdff3116e6aca56237227241f1a3
Jessica Fealy <jessicaf091@outlook.com>
2024-07-21 20:31:53 +0100

    Add trophy event

---
+++ server/Server.Domain/EventHandlers/GiveCustomerTrophyEventHandler.cs
@@ -0,0 +1,26 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Server.Domain.Events;
+
+namespace Server.Domain.EventHandlers;
+
+public class GiveCustomerTrophyEventHandler(BookRepoContext context)
+    : INotificationHandler<GiveCustomerTrophyEvent>
+{
+    public async Task Handle(
+        GiveCustomerTrophyEvent notification,
+        CancellationToken cancellationToken
+    )
+    {
+        if (
+            (await context.Customer.FindAsync([ notification.CustomerId ], cancellationToken))
+            is not { } customer
+        )
+        {
+            return;
+        }
+
+        customer.Trophies.Add(notification.Trophy);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
