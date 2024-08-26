c7ec1c88bdf9bcf6021b3e3268f2f3e97e30337a
Jessica Fealy <jessicaf091@outlook.com>
2024-08-26 22:49:22 +0100

    Close book error command

---
+++ server/Server.Domain/Commands/Admin/CloseBookErrorCommand.cs
@@ -0,0 +1,36 @@
+namespace Server.Domain.Commands.Admin;
+
+using Common.Exceptions;
+using Common.MediatR;
+using Common.Scalars.Types;
+using Server.Domain;
+using Server.Domain.Models;
+
+public class CloseBookErrorCommand : ICommand<BookRepoContext>
+{
+    public required string Isbn { get; init; }
+    public required BookErrorType Type { get; init; }
+    public required string Comment { get; init; }
+
+    public async Task Execute(
+        BookRepoContext dbContext,
+        CommandContext ctx,
+        CancellationToken cancellationToken
+    )
+    {
+        if (
+            await dbContext.BookErrors.FindAsync([ Isbn, Type ], cancellationToken)
+            is not { } bookError
+        )
+        {
+            throw new NotFoundException($"Error type {Type} for {Isbn} not found");
+        }
+
+        bookError.UpdateStatus(
+            BookErrorStatus.Closed,
+            new AdminComment() { Comment = Comment, AdminUsername = ctx.userName }
+        );
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
