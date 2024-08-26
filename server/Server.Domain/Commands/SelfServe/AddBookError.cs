4e383a7dc21dd2a8b40294f68090346957823898
Jessica Fealy <jessicaf091@outlook.com>
2024-08-26 19:44:08 +0100

    Added command + tests

---
+++ server/Server.Domain/Commands/SelfServe/AddBookError.cs
@@ -0,0 +1,40 @@
+namespace Server.Domain.Commands;
+
+using Microsoft.EntityFrameworkCore;
+using Server.Domain;
+using Server.Domain.Exceptions;
+using Server.Domain.Models;
+using Server.Domain.Scalars;
+
+public class AddBookError : ICommand<BookRepoContext>
+{
+    public required string Isbn { get; init; }
+    public string? Comment { get; init; }
+    public required BookErrorType Type { get; init; }
+
+    public async Task Execute(
+        BookRepoContext dbContext,
+        CommandContext ctx,
+        CancellationToken cancellationToken
+    )
+    {
+        if (
+            await dbContext
+                .Books
+                .Include(x => x.BookErrors)
+                .SingleOrDefaultAsync(x => x.Isbn == Isbn, cancellationToken)
+            is not { } book
+        )
+        {
+            throw new BookNotFoundException(Isbn);
+        }
+
+        if (book.BookErrors.Any(x => x.Error == Type))
+        {
+            throw new BookContainsErrorException(Isbn, Type);
+        }
+
+        book.AddError(new BookError(book, Type) { AdditionalCustomerComment = Comment });
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
