0a6fd8027264693a53ca47d1dc7ce117a4795fec
Jessica Fealy <jessicaf091@outlook.com>
2024-08-27 21:04:59 +0100

    Add executor

---
+++ server/Server/Routes/CommandExecutor.cs
@@ -0,0 +1,28 @@
+using Common.Exceptions;
+using Common.MediatR;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Server.Domain;
+
+namespace Server.Routes;
+
+public static class CommandExecutor
+{
+    public static async Task<Results<Ok, BadRequest<string>>> Execute<T>(
+        T cmd,
+        IMediator mediator,
+        CancellationToken cancellationToken
+    )
+        where T : ICommand<BookRepoContext>
+    {
+        try
+        {
+            await mediator.Send(cmd, cancellationToken);
+            return TypedResults.Ok();
+        }
+        catch (NotFoundException ex)
+        {
+            return TypedResults.BadRequest(ex.Message);
+        }
+    }
+}
