693bee2fba4435b22679be1dc6b1b7f2aa1f3172
Jessica Fealy <jessicaf091@outlook.com>
2025-01-05 11:33:37 +0000

    Add a book page

--- client/app/dashboard/add-book/page.tsx
+++ client/app/dashboard/add-book/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { Breadcrumb, ErrorSummary } from "@/components";
 import { useBookWizard } from "@/hooks";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
-import { Anchor, Button, Info, RadioButton } from "@jecfe/react-design-system";
+import { Button, RadioGroup } from "@jecfe/react-design-system";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
@@ -38,73 +39,60 @@ export default withPageAuthRequired(function AddBook() {
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-row space-x-2 pb-6">
-        <Anchor href="/dashboard">{`< Dashboard`}</Anchor>
+      <Breadcrumb
+        crumbs={[
+          { href: "/dashboard", display: "Dashboard" },
+          { display: "Choose how to add" },
+        ]}
+      />
 
-        <div className="text-slate-400 underline underline-offset-4">
-          {"< Choose how to add"}
-        </div>
-      </div>
-      <h1 className="flex flex-col text-5xl font-bold tracking-tight text-slate-200 md:text-8xl">
-        {`Choose how you'd like to add books`}
+      <h1 className="mb-8 flex flex-col text-5xl font-bold tracking-tight text-slate-200 md:text-8xl">
+        Add a book
       </h1>
-      <div className="my-4 flex max-w-sm flex-row text-xl font-bold tracking-tight text-slate-400 md:max-w-4xl md:text-3xl">
-        {`Select in what way you'd like to add books`}
-      </div>
 
       {errors.radio && (
-        <div className="flex w-full flex-col rounded-xl bg-slate-800/70 p-4 shadow-xl">
-          <div className="flex flex-row items-center space-x-4">
-            <Info className="h-10 w-10 fill-red-600" />
-            <h2 className="text-2xl font-bold text-red-600">Important!</h2>
-          </div>
-          <div className="flex flex-row pl-14 text-lg text-slate-200">
-            {errors.radio.message}
-          </div>
-        </div>
+        <ErrorSummary
+          errors={
+            errors.radio && errors.radio.message
+              ? [{ message: errors.radio.message }]
+              : undefined
+          }
+        />
       )}
 
-      <div className="flex flex-col space-x-0 space-y-4 md:flex-row md:items-center md:space-x-4 md:space-y-0">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="space-y-12 pt-12">
-            <RadioButton
-              {...register("radio", { required: "A selection is required" })}
-              value="search"
-              theme="pink"
-              size="large"
-            >
-              Search
-            </RadioButton>
-            <RadioButton
-              size="large"
-              {...register("radio", { required: "A selection is required" })}
-              value="isbn"
-              theme="cyan"
-            >
-              ISBN
-            </RadioButton>
-            <RadioButton
-              disabled
-              size="large"
-              {...register("radio", {
-                required: { value: true, message: "A selection is required" },
-              })}
-              value="csv"
-              theme="pink"
-            >
-              CSV Import
-            </RadioButton>
-          </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <RadioGroup<FormValues>
+          required
+          radioButtons={[
+            { children: "Search", theme: "cyan", value: "search" },
+            { children: "ISBN", theme: "cyan", value: "isbn" },
+            { children: "CSV Import", theme: "cyan", value: "csv" },
+          ]}
+          name={"radio"}
+          register={register}
+          legend={`Choose how you'd like to add books`}
+          hint={`Select in what way you'd like to add books`}
+          size="large"
+          errors={
+            errors.radio && errors.radio.message
+              ? [{ message: errors.radio.message }]
+              : undefined
+          }
+        />
+        <div className="mt-10 flex flex-col-reverse gap-y-4 md:flex-row md:gap-y-0 md:space-x-4">
           <Button
             size="large"
-            type="submit"
-            className="mb-10 mt-20"
-            disabled={!!errors.radio}
+            variant="secondary"
+            type="button"
+            onClick={() => router.push("/dashboard")}
           >
+            Return to dashboard
+          </Button>
+          <Button size="large" type="submit" disabled={!!errors.radio}>
             Continue
           </Button>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   );
 });
