ef51fc6361c6c7f2ff62ba915633ec39462c6996
Jessica Fealy <jessicaf091@outlook.com>
2026-05-24 19:27:09 +0100

    feat: improving account managemt flow

--- client/components/modal/Modal.tsx
+++ client/components/modal/Modal.tsx
@@ -26,12 +26,14 @@ export function Modal({
       {isOpen && (
         <>
           <div className="fixed left-0 top-0 z-40 h-full w-full bg-slate-900/80" />
-          <div className="fixed left-1/2 top-1/2 z-50 w-11/12 max-w-lg max-h-[85vh] -translate-x-1/2 -translate-y-1/2 overflow-auto rounded-xl bg-slate-300 p-6 shadow-2xl md:p-12">
+          <div className="fixed left-1/2 top-1/2 z-50 max-h-[85vh] w-11/12 max-w-lg -translate-x-1/2 -translate-y-1/2 overflow-auto rounded-xl bg-slate-300 p-6 shadow-2xl md:p-12">
             <div className="flex w-full flex-col">
               {children}
               <div className="mt-8 border-t border-slate-400" />
               {error && (
-                <div className="flex pb-4 pt-2 text-sm text-red-600">{error}</div>
+                <div className="flex pb-4 pt-2 text-sm text-red-600">
+                  {error}
+                </div>
               )}
               <div className="mt-4 flex flex-row items-center pb-2">
                 <Anchor
